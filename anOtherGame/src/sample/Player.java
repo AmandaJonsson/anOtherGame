8453bfde2868ebdfa55de306b38c87708d8542ec
MajaNyberg <marnybe@student.chalmers.se>
2017-04-03 13:34:50 +0200

    added PlayerController, wrote some code in Player

--- anOtherGame/src/sample/Player.java
+++ anOtherGame/src/sample/Player.java
@@ -2,4 +2,39 @@ package sample;
 
 
 public class Player {
+
+    private String position;
+    private boolean hasTramCard = false;
+    private int budget = 5000;      //StartBudget is 5000 kr right?
+
+    public Player(String position, boolean hasTramCard, int budget){
+        this.position = position;
+        this.hasTramCard = hasTramCard;
+        this.budget = budget;
+    }
+
+    public int getBudget(){
+        return this.budget;
+    }
+
+    public String getPosition(){
+        return this.position;
+    }
+
+    public void setPosition(String newPosition){ //onödig när vi har den här i PlayerController?
+        this.position = newPosition;
+
+    }
+
+    public void gotTramCard(){          //ska också ligga i  PlayerController?
+        hasTramCard = true;
+    }
+
+    public void usedTramCard(){         //ska också ligga i PlayerController?
+        hasTramCard = false;
+    }
+
+
+
+
 }

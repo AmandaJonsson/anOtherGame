3913ccf670c4144a8bc25ee42b16560417e36c53
Amanda Jonsson <amandaj9510@gmail.com>
2017-04-03 13:40:32 +0200

    Implemented a Dice class

--- anOtherGame/src/sample/Dice.java
+++ anOtherGame/src/sample/Dice.java
@@ -1,5 +1,19 @@
 package sample;
 
 
+import java.util.Random;
+
 public class Dice {
+
+    public int die;
+
+    public Dice(){
+        roll();
+    }
+
+    public void roll(){
+        Random randomNumber = new Random();
+        die = randomNumber.nextInt(6)+1;
+        System.out.println(die);
+    }
 }

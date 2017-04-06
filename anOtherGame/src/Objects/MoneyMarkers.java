455211aabbef5e87cc5666d941ce39925b538d3e
Amanda Jonsson <amandaj9510@gmail.com>
2017-04-06 13:51:14 +0200

    adding moneymarker

---
+++ anOtherGame/src/Objects/MoneyMarkers.java
@@ -0,0 +1,33 @@
+package Objects;
+
+/**
+ * Created by amandajonsson on 2017-04-06.
+ */
+public class MoneyMarkers implements Markers {
+
+    int n;
+
+
+    public MoneyMarkers(){
+
+    }
+
+    public int topaz (){
+
+        return 0;
+
+
+    }
+
+    public int emerald (){
+
+        return 0;
+    }
+
+    public int ruby (){
+
+        return 0;
+    }
+
+
+}

9240d0c48783f744b0c957fcdaeca3491f22c457
Miranda Bånnsgård <bannsgard@gmail.com>
2017-04-08 17:52:45 +0200

    Testing tests of classes Dice and Player

---
+++ anOtherGame/tests/Objects/DiceTest.java
@@ -0,0 +1,14 @@
+package Objects;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    @Test
+    public void testRoll() throws Exception {
+        Dice dice = new Dice();
+        assertTrue(dice.roll() > 0 || dice.roll() < 7);
+    }
+}
---
+++ anOtherGame/tests/Objects/PlayerTest.java
@@ -0,0 +1,33 @@
+package Objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetBudget() throws Exception {
+        
+    }
+
+    @Test
+    public void testGetPosition() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPosition() throws Exception {
+
+    }
+
+    @Test
+    public void testGotTramCard() throws Exception {
+
+    }
+
+    @Test
+    public void testUsedTramCard() throws Exception {
+
+    }
+}

e91ec35496aa8176c02bb12c5b622ac24cefde38
MajaNyberg <marnybe@student.chalmers.se>
2017-04-12 14:30:24 +0200

    added kitten class

---
+++ anOtherGame/src/TheLostKitten.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class TheLostKitten {
+
+    private LinkedList players = new LinkedList();
+
+    public TheLostKitten(){
+        Objects.Map map = new Objects.Map();
+        Objects.Dice dice = new Objects.Dice();
+
+    }
+
+    public void nextTurn(){
+
+    }
+
+
+}

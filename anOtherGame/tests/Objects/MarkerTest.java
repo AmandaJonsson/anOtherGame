64c40762d900d8f48246422dd70f100cd597c913
Amanda Jonsson <amandaj9510@gmail.com>
2017-05-03 13:17:34 +0200

    Markertest

---
+++ anOtherGame/tests/Objects/MarkerTest.java
@@ -0,0 +1,17 @@
+package Objects;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by amandajonsson on 2017-05-02.
+ */
+public class MarkerTest {
+
+    @Test
+    public void testMarker(){
+        MoneyMarker mark = new MoneyMarker(MoneyMarker.TypeOfMarkers.TOPAZ);
+        assertTrue(mark.getMarkerType() == MoneyMarker.TypeOfMarkers.TOPAZ);
+        assertFalse(mark.getMarkerType()== MoneyMarker.TypeOfMarkers.RUBY);
+
+        }
+}

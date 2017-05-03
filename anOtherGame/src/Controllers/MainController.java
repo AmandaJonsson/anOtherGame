e4a2d77fa36b8ddf021ff0d5f933f8b712feead7
MajaNyberg <marnybe@student.chalmers.se>
2017-05-03 15:15:39 +0200

    new controller and rules pane

---
+++ anOtherGame/src/Controllers/MainController.java
@@ -0,0 +1,126 @@
+package Controllers;
+
+import Design.MapView;
+import Objects.Map;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+/**
+ * Created by majanyberg on 2017-05-03.
+ */
+public class MainController {
+
+    @FXML
+    private Button rulesButton = new Button();
+    @FXML
+    private Button startGameButton = new Button();
+    @FXML
+    private Button backToStartButton = new Button();
+    @FXML
+    private TextField playerTextField1, playerTextField2,
+            playerTextField3, playerTextField4, playerTextField5, playerTextField6;
+
+    @FXML protected void handleStartGameButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage) startGameButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("/Design/TheLostPane.fxml"));
+        VBox playerPlace = (VBox)root.lookup("#playerPlace");
+
+        Map map = new Map();
+        MapView mapView = new MapView();
+        MapController controller = new MapController(map, mapView);
+        Pane mapPlace = (AnchorPane)root.lookup("#mapPlace");
+
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+
+
+        AnchorPane.setTopAnchor(mapView, 0.0);
+        AnchorPane.setLeftAnchor(mapView, 0.0);
+        AnchorPane.setRightAnchor(mapView, 0.0);
+        AnchorPane.setBottomAnchor(mapView, 0.0);
+
+
+        mapPlace.getChildren().add(mapView);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    /*Parent root = FXMLLoader.load(getClass().getResource("/Design/theLostPane.fxml"));
+        primaryStage.setTitle("The Lost Kitten");
+        AnchorPane.setTopAnchor(root, 0.0);
+        AnchorPane.setLeftAnchor(root, 0.0);
+        AnchorPane.setRightAnchor(root, 0.0);
+        AnchorPane.setBottomAnchor(root, 0.0);
+        AnchorPane pane = new AnchorPane();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        Map map = new Map();
+        MapView mapView = new MapView();
+        MapController controller = new MapController(map, mapView);
+        Pane mapPlace = (AnchorPane)root.lookup("#mapPlace");
+        VBox playerPlace = (VBox)root.lookup("#playerPlace");
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        playerPlace.getChildren().add(FXMLLoader.load(getClass().getResource("/Design/PlayerPane.fxml")));
+        //mapPlace.getChildren().add(mapView);
+        //pane.getChildren().add(mapView);
+        pane.getChildren().add(root);
+        AnchorPane.setTopAnchor(mapView, 0.0);
+        AnchorPane.setLeftAnchor(mapView, 0.0);
+        AnchorPane.setRightAnchor(mapView, 0.0);
+        AnchorPane.setBottomAnchor(mapView, 0.0);
+        mapPlace.getChildren().add(mapView);
+        primaryStage.show();*/
+
+
+
+
+
+
+    @FXML protected void handleRulesGameButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage) rulesButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        Pane theLostPane = (Pane) loader.load(getClass().getResource("/Design/rulesPane.fxml"));
+
+        Scene scene = new Scene(theLostPane);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+    @FXML protected void handleBackToStartButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage) backToStartButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        Pane theLostPane = (Pane) loader.load(getClass().getResource("/Design/start.fxml"));
+        Scene scene = new Scene(theLostPane);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+}

852a885a8153fc4c6d8b0575693d9a3f662c8912
MajaNyberg <marnybe@student.chalmers.se>
2017-05-09 23:31:23 +0200

    tried to fix the budgets, didnt quite manage

--- anOtherGame/src/Controller/MainController.java
+++ anOtherGame/src/Controller/MainController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+
 import Model.Player;
 import Model.TheLostKitten;
 import View.MapView;
@@ -7,7 +8,6 @@ import Model.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -38,6 +38,16 @@ public class MainController {
     private TextField playerTextField1, playerTextField2,
             playerTextField3, playerTextField4, playerTextField5, playerTextField6;
 
+    TheLostKitten newGame;
+
+    //Creating textfields for the players budgets
+
+    Label budgetLabel1 = new Label("BUDGET 1");
+    Label budgetLabel2 = new Label("BUDGET 2");
+    Label budgetLabel3 = new Label("BUDGET 3");
+    Label budgetLabel4 = new Label("BUDGET 4");
+    Label budgetLabel5 = new Label("BUDGET 5");
+    Label budgetLabel6 = new Label("BUDGET 6");
 
 
     @FXML protected void handleStartGameButton(ActionEvent event) throws IOException {
@@ -71,6 +81,9 @@ public class MainController {
         Label playerLabel5 = new Label();
         Label playerLabel6 = new Label();
 
+
+
+
         AnchorPane.setTopAnchor(mapView, 0.0);
         AnchorPane.setLeftAnchor(mapView, 0.0);
         AnchorPane.setRightAnchor(mapView, 0.0);
@@ -82,69 +95,123 @@ public class MainController {
         ArrayList<String> players = new ArrayList<String>();
 
 
+
             if (!playerTextField1.getText().isEmpty()) {
                 players.add(playerTextField1.getText());
                 playerVBox.getChildren().add(playerPane1);
+
+                //adds name label to pane
                 playerPane1.getChildren().addAll(playerLabel1);
                 playerLabel1.setLayoutX(115);
                 playerLabel1.setLayoutY(20);
                 playerLabel1.setText(playerTextField1.getText());
                 playerLabel1.setFont(new Font("Comic sans", 15));
 
+                //adds budget label to pane
+                playerPane1.getChildren().addAll(budgetLabel1);
+                budgetLabel1.setLayoutX(10);
+                budgetLabel1.setLayoutY(40);
+                budgetLabel1.setFont(new Font("Comic sans", 10));
+
+
             }
 
             if (!playerTextField2.getText().isEmpty()) {
                 players.add(playerTextField2.getText());
                 playerVBox.getChildren().add(playerPane2);
+
+                //adds name label to pane
                 playerPane2.getChildren().addAll(playerLabel2);
                 playerLabel2.setLayoutX(115);
                 playerLabel2.setLayoutY(20);
                 playerLabel2.setText(playerTextField2.getText());
                 playerLabel2.setFont(new Font("Comic sans", 15));
 
+                //adds budget label to pane
+                playerPane2.getChildren().addAll(budgetLabel2);
+                budgetLabel2.setLayoutX(10);
+                budgetLabel2.setLayoutY(40);
+                budgetLabel2.setFont(new Font("Comic sans", 10));
+
+
             }
 
             if (!playerTextField3.getText().isEmpty()) {
                 players.add(playerTextField3.getText());
                 playerVBox.getChildren().add(playerPane3);
+
+                //adds name label to pane
                 playerPane3.getChildren().addAll(playerLabel3);
                 playerLabel3.setLayoutX(115);
                 playerLabel3.setLayoutY(20);
                 playerLabel3.setText(playerTextField3.getText());
                 playerLabel3.setFont(new Font("Comic sans", 15));
 
+                //adds budget label to pane
+                playerPane3.getChildren().addAll(budgetLabel3);
+                budgetLabel3.setLayoutX(10);
+                budgetLabel3.setLayoutY(40);
+                budgetLabel3.setFont(new Font("Comic sans", 10));
+
+
             }
 
             if (!playerTextField4.getText().isEmpty()) {
                 players.add(playerTextField4.getText());
                 playerVBox.getChildren().add(playerPane4);
+
+                //adds name label to pane
                 playerPane4.getChildren().addAll(playerLabel4);
                 playerLabel4.setLayoutX(115);
                 playerLabel4.setLayoutY(20);
                 playerLabel4.setText(playerTextField4.getText());
                 playerLabel4.setFont(new Font("Comic sans", 15));
 
+                //adds budget label to pane
+                playerPane4.getChildren().addAll(budgetLabel4);
+                budgetLabel4.setLayoutX(10);
+                budgetLabel4.setLayoutY(40);
+                budgetLabel4.setFont(new Font("Comic sans", 10));
+
             }
 
             if (!playerTextField5.getText().isEmpty()) {
                 players.add(playerTextField5.getText());
                 playerVBox.getChildren().add(playerPane5);
+
+                //adds name label to pane
                 playerPane5.getChildren().addAll(playerLabel5);
                 playerLabel5.setLayoutX(115);
                 playerLabel5.setLayoutY(20);
                 playerLabel5.setText(playerTextField5.getText());
                 playerLabel5.setFont(new Font("Comic sans", 15));
 
+                //adds budget label to pane
+                playerPane5.getChildren().addAll(budgetLabel5);
+                budgetLabel5.setLayoutX(10);
+                budgetLabel5.setLayoutY(40);
+                budgetLabel5.setFont(new Font("Comic sans", 10));
+
+
             }
 
             if (!playerTextField6.getText().isEmpty()) {
                 players.add(playerTextField6.getText());
                 playerVBox.getChildren().add(playerPane6);
+
+                //adds name label to pane
                 playerPane6.getChildren().addAll(playerLabel6);
                 playerLabel6.setLayoutX(115);
                 playerLabel6.setLayoutY(20);
                 playerLabel6.setText(playerTextField6.getText());
                 playerLabel6.setFont(new Font("Comic sans", 15));
+
+                //adds budget label to pane
+                playerPane6.getChildren().addAll(budgetLabel6);
+                budgetLabel6.setLayoutX(10);
+                budgetLabel6.setLayoutY(40);
+                budgetLabel6.setFont(new Font("Comic sans", 10));
+
 
             }
 
@@ -177,6 +244,12 @@ public class MainController {
 
         }
 
+        newGame = new TheLostKitten(players);
+
+//        theLostController newController = new theLostController(newGame);
+
+
+
         if(hasSameName == false){
             Scene scene = new Scene(root);
             stage.setScene(scene);
@@ -184,8 +257,6 @@ public class MainController {
         }
 
 
-        TheLostKitten newGame = new TheLostKitten(players);
-        //theLostController newController = new theLostController(newGame);
 
     }
 

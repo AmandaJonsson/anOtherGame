9dfd1dfecfb8b85f3bdde85ec7d3d8e4d36adb5d
Miranda Bånnsgård <bannsgard@gmail.com>
2017-05-10 13:31:13 +0200

    Added a new class, FindPath that will be able to give potential spaces for a player to visit after player has rolled the dice

---
+++ anOtherGame/src/Model/FindPath.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPath {
+    private List<ISpace> spaces;
+    private List<ISpace> potentialSpaces;
+    private List<ISpace> visitedSpaces;
+
+    public FindPath(Map map){
+        this.spaces = new ArrayList<ISpace>(map.getSpaces());
+    }
+
+
+
+
+
+}

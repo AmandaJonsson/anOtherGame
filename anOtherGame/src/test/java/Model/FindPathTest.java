d55114e594c36718e8d5b72d8c58bc4840824d90
Miranda Bånnsgård <bannsgard@gmail.com>
2017-05-28 18:04:19 +0200

    Added method findPotentialStations to test

--- anOtherGame/src/test/java/Model/FindPathTest.java
+++ anOtherGame/src/test/java/Model/FindPathTest.java
@@ -12,22 +12,30 @@ import Mocks.MockMap;
 import Model.Intefaces.IMap;
 import Model.Intefaces.IStation;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 
 public class FindPathTest {
+    
     @Test
     public void findPotentialStations() throws Exception {
         IMap map1 = new MockMap();
-        FindPath findPath = new FindPath();
-        findPath.findPotentialStations((Station) map1.getSpaces().get(3));
-        System.out.println(findPath.findPotentialStations((Station) map1.getSpaces().get(3)));
 
+        FindPath findPath1 = new FindPath();
+        List<IStation> listOfStations1 = findPath1.findPotentialStations((Station) map1.getSpaces().get(3));
+        assertTrue(listOfStations1.get(0).compareSpaces(map1.getSpaces().get(9)));
+        assertTrue(listOfStations1.get(1).compareSpaces(map1.getSpaces().get(10)));
 
-        if(map1.getSpaces().get(3) instanceof IStation){
-            System.out.println("hej");
-        }
+        FindPath findPath2 = new FindPath();
+        List<IStation> listOfStations2 = findPath2.findPotentialStations((Station) map1.getSpaces().get(9));
+        assertTrue(listOfStations2.get(0).compareSpaces(map1.getSpaces().get(3)));
 
+        FindPath findPath3 = new FindPath();
+        List<IStation> listOfStations3 = findPath3.findPotentialStations((Station) map1.getSpaces().get(10));
+        assertTrue(listOfStations3.get(0).compareSpaces(map1.getSpaces().get(3)));
     }
 
     @Test
